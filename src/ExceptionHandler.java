1987afc2faf6c871044343837e350a0b5df0f8bf
sydneycodes22 <sydneycodes22@gmail.com>
2025-12-17 16:42:18 -0800

    initialize files for assignment 4

---
+++ src/ExceptionHandler.java
@@ -0,0 +1,9 @@
+public class ExceptionHandler {
+    public static void main(String[] args) {
+        try {
+            System.out.println("Hello, World!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
